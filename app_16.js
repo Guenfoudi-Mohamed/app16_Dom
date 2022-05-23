8b0af892f50533867e2d0c0e171978cf170de301
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-05-23 14:19:23 +0100

    Update app_16.js

--- app_16.js
+++ app_16.js
@@ -1,15 +1,14 @@
 // app_16
-
 const items = document.querySelectorAll('body #section .container .item');
 const playerTurn = document.querySelector('body #section .playerTurn');
 const popUp = document.querySelector('body #popUp');
-
 const arrXO = ['X','O'];
 let turnXO = arrXO[Math.floor(Math.random() * 2)];
 let arr0 = [];
 let arr1 = [];
 let numberOfClicks = 0;
 
+// function checkResult 
 function checkResult(XO,index){
     const arr = ['012','345','678','036','147','258','048','246'];
     let list = [];
@@ -17,6 +16,7 @@ function checkResult(XO,index){
     let stock = '';
     let conteur = 0;
     numberOfClicks++;
+    // start function displayPopUp 
     function displayPopUp(){
         popUp.style.setProperty('display','flex');
         const btnRestart = document.querySelector('body #popUp .btnRestart');
@@ -70,6 +70,7 @@ function checkResult(XO,index){
     if(bool == true){
         bool=false;
         playerTurn.textContent = `Player ${XO} Win !`;
+        popUp.children[0].textContent = `Player ${XO} Win !`;
         for(let i = 0;i<stock.length;i++){
             items[Number(stock[i])].style.cssText = `background-color:#00d500;`;
         }
@@ -77,45 +78,49 @@ function checkResult(XO,index){
     }
     else{
         if(numberOfClicks === 9){
+            popUp.children[0].textContent = `Draw !`;
             displayPopUp();
         }
         if(XO == 'X'){XO = 'O';playerTurn.textContent = `TURN Player ${XO}`;}
         else if(XO == 'O'){XO = 'X';playerTurn.textContent = `TURN Player ${XO}`;}
     };
 }
-
+// function mouseOver
+function mouseOver(){
+    if(this.children[0].textContent == ''){
+        this.children[0].textContent = `${turnXO}`;
+    }
+}
+// function mouseOut
+function mouseOut(){
+    if(this.children[0].textContent.length != 0){
+        this.children[0].textContent = '';
+    }
+}
+// function mouseClick
+function mouseClick(){
+    if((this.children[0].classList.contains('active')) == false){
+        this.removeEventListener('mouseover',arr0[Number(this.attributes[1].value)]);
+        this.removeEventListener('mouseout',arr1[Number(this.attributes[1].value)]);
+        this.children[0].classList.add('active');
+        this.children[0].textContent = `${turnXO}`;
+        if(turnXO == 'X'){
+            checkResult(turnXO,Number(this.attributes[1].value));
+            turnXO = 'O';
+        }
+        else if(turnXO == 'O'){
+            checkResult(turnXO,Number(this.attributes[1].value));
+            turnXO = 'X';
+        }
+    }
+}
+// playerTurn initial value 
+playerTurn.textContent = `TURN Player ${turnXO}`;
 
 for(let i = 0;i<items.length;i++){
-    playerTurn.textContent = `TURN Player ${turnXO}`;
-    function mouseOver(){
-        if(items[i].children[0].textContent == ''){
-            items[i].children[0].textContent = `${turnXO}`;
-        }
-    }
-    function mouseOut(){
-        if(items[i].children[0].textContent.length != 0){
-            items[i].children[0].textContent = '';
-        }
-    }
-    function mouseClick(){
-        if((items[i].children[0].classList.contains('active')) == false){
-            items[i].removeEventListener('mouseover',mouseOver);
-            items[i].removeEventListener('mouseout',mouseOut);
-            items[i].children[0].classList.add('active');
-            items[i].children[0].textContent = `${turnXO}`;
-            if(turnXO == 'X'){
-                checkResult(turnXO,i);
-                turnXO = 'O';
-            }
-            else if(turnXO == 'O'){
-                checkResult(turnXO,i);
-                turnXO = 'X';
-            }
-        }
-    }
     arr0.push(mouseOver);
-    arr1.push(mouseOut);
     items[i].addEventListener('mouseover',mouseOver);
+    arr1.push(mouseOut);
     items[i].addEventListener('mouseout',mouseOut);
     items[i].addEventListener('click',mouseClick);
-}
+};
